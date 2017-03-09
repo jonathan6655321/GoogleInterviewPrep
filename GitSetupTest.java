86d99ffdf5e6478cf34135741c5655d3e757ddab
ido kessler <ido kessler>
2017-03-09 16:01:21 +0200

    the interface

--- GitSetupTest.java
+++ GitSetupTest.java
@@ -1,12 +1,10 @@
-package GoogleInterviewPrep;
-
 public class GitSetupTest {
 	public static interface GraphInterface{
 		void insert(int[] arr);
 		boolean connected(int a, int b);
 	}
 	
-	public static boolean test1(){
+	public static boolean test1(GraphInterface g){
 		
 	}
 }
